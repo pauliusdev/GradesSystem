79e01862dde87e940975b74c98869471dee32d55
pauliusdev <pauliusbucks@gmail.com>
2018-05-11 00:15:02 +0100

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades 
+{
+    class GradeBook 
+    {
+        public string number;
+        List<float> grades = new List<float>();
+        public void AddGrade(float grade) 
+        {
+            Console.Write("Enter grade: ");
+            number = Console.ReadLine();
+
+            grades.Add(grade);
+            grades.Add(float.Parse(number));
+            grades.Add(232f);
+            foreach (var item in grades) {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
